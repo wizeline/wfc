2768acc7d32c7d72e9c94ee5e0554b4c792478e4
Diego Guzmán <diego.guzman@wizeline.com>
2018-05-03 14:25:06 -0500

    Add test case

--- tests/test_control.py
+++ tests/test_control.py
@@ -10,3 +10,6 @@ class TestControl(CompilerTestCase):
     def test_control_with_bad_syntax(self):
         with self.assertRaises(errors.ParseError):
             self._compile_to_json('control-with-bad-syntax')
+
+    def test_has_entity(self):
+        self._compile_to_json('has-entity')
